ef0a0ac5277c70d50158c9ed56f56fd70ccc7f8a
caio2313 <118941717+caio2313@users.noreply.github.com>
2023-12-06 15:14:44 -0300

    Add files via upload

---
+++ hockey/paddle.py
@@ -0,0 +1,50 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, x):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.setposition(x=x)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.home()
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.home()
+        self.move_speed = 0.1
+        self.bounce_x()
